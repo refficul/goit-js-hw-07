83a3e89e1e21852ac32cfc687a10f2006c701cd1
refficul <vic.w.toria.q@gmail.com>
2023-09-09 12:01:56 +0300

    main

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+gallery.insertAdjacentHTML(
+  "afterbegin",
+  galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description},"
+    />
+  </a>
+</div>`;
+    })
+    .join("")
+);
+
+function openModalImg(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `
+      <img src="${event.target.getAttribute("data-source")}">,
+  `,
+    {
+      onShow: () => window.addEventListener("keydown", onEsc),
+      onClose: () => window.removeEventListener("keydown", onEsc),
+    }
+  );
+  const onEsc = (event) => {
+    if (event.code === "Escape") instance.close();
+  };
+  instance.show();
+}
+
+gallery.addEventListener("click", openModalImg);
